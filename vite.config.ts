e14622f1aa9981956102165421b00c6836708775
omarkamal62 <okamal34@gmail.com>
2025-05-15 18:06:02 +0300

    using .env file in the config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in development
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL || "http://localhost:3000",
+          changeOrigin: true,
+        },
       },
     },
-  },
+  };
 });
